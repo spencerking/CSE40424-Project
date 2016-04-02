fba184b2f7eb0d74ad6c96e89ae4e1e1cbdc4df5
Sam Rack <srack@nd.edu>
2016-04-02 15:15:52 -0400

    added book images to home screen, only first causes navigation when clicked

--- js/gestures.js
+++ js/gestures.js
@@ -41,6 +41,6 @@ function gestures(rightHammer, leftHammer, bookHammer) {
 		document.getElementById('bookmarks_bar').style.visibility = "visible";
 		document.getElementById('right_bar').style.visibility = "visible";
 		document.getElementById('left_bar').style.visibility = "visible";
-		document.getElementById('book_title').style.display = "block";
+		document.getElementById('header_title').innerHTML = "Sample Book";
 	});
 }
