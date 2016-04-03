47530872085bc618057b83de473bb53020556923
Sam Rack <srack@nd.edu>
2016-04-02 22:25:12 -0400

    added gestures for swiping up on bookmark bar, returning to homepage

--- js/gestures.js
+++ js/gestures.js
@@ -1,4 +1,4 @@
-function gestures(rightHammer, leftHammer, bookHammer) {
+function gestures() {
 
 	// Old test code, kept for now for basic examples
 	// hammertime.on('tap', function(ev) {
@@ -22,25 +22,68 @@ function gestures(rightHammer, leftHammer, bookHammer) {
 	// 	console.log('press');
 	// });
 
-	// hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+	var myOptions = null;
 
-	rightHammer.on('swipeleft', function(ev) {
-		console.log('advance page');
-		document.getElementById('ipad_background').style.backgroundColor = "red";
-	});
+	// swipe to advance to the net page
+	var rightHammer = new Hammer(document.getElementById('right_bar'), myOptions);
+	rightHammer.on('swipeleft', function(ev) { advance_page(); });
 
-	leftHammer.on('swiperight', function(ev) {
-		console.log('move back page');
-		document.getElementById('ipad_background').style.backgroundColor = "green";
-	});
+	// swipe to go back a page
+	var leftHammer = new Hammer(document.getElementById('left_bar'), myOptions);
+	leftHammer.on('swiperight', function(ev) { back_a_page(); });
 
-	bookHammer.on('tap', function(ev) {
-		console.log('open Book');
-		document.getElementById('main_page').style.visibility = "hidden";
-		document.getElementById('book_page').style.visibility = "visible";
-		document.getElementById('bookmarks_bar').style.visibility = "visible";
-		document.getElementById('right_bar').style.visibility = "visible";
-		document.getElementById('left_bar').style.visibility = "visible";
-		document.getElementById('header_title').innerHTML = "Sample Book";
-	});
+	// books opening from homepage
+	var bookElements = document.getElementsByClassName('book_img'); // Might not need to worry about generalizing this for the prototype
+	var book0Hammer = new Hammer(bookElements[0], myOptions);
+	var book1Hammer = new Hammer(bookElements[1], myOptions);
+	var book2Hammer = new Hammer(bookElements[2], myOptions);
+	book0Hammer.on('tap', function(ev) { open_book(); });
+	book1Hammer.on('tap', function(ev) { open_book(); });
+	book2Hammer.on('tap', function(ev) { open_book(); });
+	
+	// return to home page
+	var homeHammer = new Hammer(document.getElementById('home_button'), myOptions);
+	homeHammer.on('tap', function(ev) { go_to_home(); });
+	
+	// expand bookmarks_bar on swiping updateCommands
+	var bookmarksHammer = new Hammer(document.getElementById('bookmarks_bar'), myOptions);
+	bookmarksHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+	bookmarksHammer.on('swipeup', function(ev) { expand_bookmarks_bar(); });
+}
+
+function advance_page() {
+	console.log('advance page');
+	document.getElementById('book_page').style.backgroundColor = "red";
+}
+
+function back_a_page() {
+	console.log('move back page');
+	document.getElementById('book_page').style.backgroundColor = "green";	
+}
+
+function open_book() {
+	console.log('open Book');
+	document.getElementById('main_page').style.visibility = "hidden";
+	document.getElementById('book_page').style.visibility = "visible";
+	document.getElementById('bookmarks_bar').style.visibility = "visible";
+	document.getElementById('right_bar').style.visibility = "visible";
+	document.getElementById('left_bar').style.visibility = "visible";
+	document.getElementById('header_title').innerHTML = "Sample Book";
+}
+
+function go_to_home() {
+	console.log('return to home page')
+	// basically just undo everything that open_book does 
+	// NOTE: later will probably have to add more to remove settings if open, etc
+	document.getElementById('main_page').style.visibility = "visible";
+	document.getElementById('book_page').style.visibility = "hidden";
+	document.getElementById('bookmarks_bar').style.visibility = "hidden";
+	document.getElementById('right_bar').style.visibility = "hidden";
+	document.getElementById('left_bar').style.visibility = "hidden";
+	document.getElementById('header_title').innerHTML = "e(njoyable)Textbooks";
+}
+
+function expand_bookmarks_bar() {
+	console.log('expanding bookmarks bar upward');
+	document.getElementById('bookmarks_bar').style.backgroundColor = "yellow";
 }
